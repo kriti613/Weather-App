51b4cb5b460dd239aeb9b99abe22a829a02ca265
Kriti Gupta <kg26071999@gmail.com>
2021-08-16 21:57:23 +0530

    Adding All Files

---
+++ script.js
@@ -0,0 +1,33 @@
+let weather = {
+  "apiKey": "ee759a66e8b195155fcdda0af9c02da9",
+  fetchWeather: function(city){
+    fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=matrix&appid=" + this.apiKey).then((response) => response.json()).then((data) => this.displayWeather(data));
+  },
+  displayWeather: function(data){
+    const { name } = data;
+    const { icon, description} = data.weather[0];//Because it is stored in the form of an array
+    const { temp, humidity } = data.main;
+    const { speed } = data.wind;
+    document.querySelector(".city").innerText = "Weather in " + name;
+    document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + ".png";
+    document.querySelector(".description").innerText = description;
+    document.querySelector(".temp").innerText = temp + "°C";
+    document.querySelector(".humidity").innerText ="Humidity: " + humidity + "%";
+    document.querySelector(".wind").innerText = "Wind Speed: " + speed + "km/h";
+    document.querySelector(".weather").classList.remove("loading");
+    document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?" + name + "')";
+  },
+  search: function(){
+    this.fetchWeather(document.querySelector(".search-bar").value);
+  }
+};
+
+document.querySelector(".search button").addEventListener("click", function(){weather.search();});
+document.querySelector(".search-bar").addEventListener("keyup", function (event){
+  if(event.key == "Enter"){
+    weather.search();
+  }
+});
+
+// To load the page
+weather.fetchWeather("Delhi");
